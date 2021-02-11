bf0781845647acc99eb62e8594eeda0537296d58
JosiasAurel <ndjosiasaurel@gmail.com>
2021-02-11 19:14:06 +0100

    add redirect ability

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
-import React, { FunctionComponent } from "react"
+import { redirect } from "next/dist/next-server/server/api-utils"
+import React, { FunctionComponent, useState } from "react"
 import styles from "./index.module.css"
 
 const Index: FunctionComponent = (): JSX.Element => {
+    const [profile, setProfile] = useState("")
+    const formSubmit = (e) => {
+        e.preventDefault()
+        location.pathname = (`/${profile}`)
+    }
     return (
         <div className={styles.mainIndexPage}>
-            <div>
+            <form onSubmit={formSubmit}>
                 <h2 className={styles.title}>Get your GitHub Profile Card</h2>
-                <input className={styles.username} type="text" placeholder="Enter your GitHub username e.g JosiasAurel"/>
-            </div>
+                <input 
+                value={profile} 
+                className={styles.username} 
+                type="text" 
+                placeholder="Enter your GitHub username e.g JosiasAurel"
+                onChange={(e) => setProfile(e.target.value)}
+                />
+            </form>
         </div>
     )
 }
